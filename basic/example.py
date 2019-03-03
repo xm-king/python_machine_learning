579972049ead30684c34bbf9cccc0a35fc587dda
pingchun <pingchun@mogujie.com>
2019-03-03 14:06:36 +0800

    basci 基础知识

---
+++ basic/example.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+print("Hello, Python!")
+print "Hello, Python!"
+
+if True:
+    print "True"
+else:
+    print "False "
+
+days = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+print days
