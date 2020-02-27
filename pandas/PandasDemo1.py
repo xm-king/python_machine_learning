be82f5c3bbe4d7d96eed3d9ed84cbc66b99311ca
pingchun <pingchun@gegejia.com>
2020-02-27 10:41:32 +0800

    commit

---
+++ pandas/PandasDemo1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+### Panda索引
+# data = pd.read_csv("titanic_train.csv")
+# print(data.head())
+# print(data[['Age',"Fare"]][:5])
+# print((data.index))
+# print(data.iloc[0:5])
+# print((data['Age']>70).sum())
+
+### GroupBy操作
+df = pd.DataFrame({"Key":['A','B',"C",'A','B',"C",'A','B',"C"],
+                   "data":[1,2,3,6,2,3,4,2,2]})
+print(df.groupby)
---
+++ regression/logistis_regression.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from scipy.optimize import minimize
+from sklearn.preprocessing  import PolynomialFeatures
+
+pd.set_option('display.notebook_repr_html', False)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 150)
+pd.set_option('display.max_seq_items', None)
