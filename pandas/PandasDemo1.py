3d726a04c438cc666b35c4d239bccfee6a0ab6ef
pingchun <pingchun@mogujie.com>
2020-03-03 23:50:37 +0800

    pandas

--- pandas/PandasDemo1.py
+++ pandas/PandasDemo1.py
@@ -1,11 +1,12 @@
 import pandas as pd
 ### Panda索引
-# data = pd.read_csv("titanic_train.csv")
-# print(data.head())
-# print(data[['Age',"Fare"]][:5])
-# print((data.index))
-# print(data.iloc[0:5])
-# print((data['Age']>70).sum())
+data = pd.read_csv("titanic_train.csv")
+print("Data: %s,dType:%s"%(type(data),data.dtypes))
+print(data.head())
+print(data[['Age',"Fare"]][:5])
+print((data.index))
+print(data.iloc[0:5])
+print((data['Age']>70).sum())
 
 ### GroupBy操作
 df = pd.DataFrame({"Key":['A','B',"C",'A','B',"C",'A','B',"C"],
